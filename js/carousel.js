91e87fcc58a3d036469e47ebf1c9ca44297cfa3e
shljshlj <34565607+shljshlj@users.noreply.github.com>
2019-12-27 21:09:58 +0100

    add fade-in and fade-out animation

---
+++ js/carousel.js
@@ -0,0 +1,50 @@
+let items = document.querySelectorAll('.gallery-item');
+let currentItem = 0;
+let isEnabled = true;
+
+function changeCurrentItem(n) {
+    currentItem = (n + items.length) % items.length;
+}
+
+function hideItem(direction) {
+    isEnabled = false;
+    items[currentItem].classList.add(direction);
+    items[currentItem].addEventListener('animationend', function () {
+        this.classList.remove('active', direction);
+    });
+}
+
+function showItem(direction) {
+    items[currentItem].classList.add('next', direction);
+    items[currentItem].addEventListener('animationend', function () {
+        this.classList.remove('next', direction);
+        this.classList.add('active');
+        isEnabled = true;
+    });
+}
+
+function previousItem(n) {
+    hideItem('to-right');
+    changeCurrentItem(n - 1);
+    showItem('from-left');
+}
+
+function nextItem(n) {
+    hideItem('to-left');
+    changeCurrentItem(n + 1);
+    showItem('from-right');
+}
+
+document.querySelector('.overlay-controls.left-control').addEventListener('click', function () {
+    if (isEnabled) {
+        previousItem(currentItem);
+    }
+});
+
+document.querySelector('.overlay-controls.right-control').addEventListener('click', function () {
+    if (isEnabled) {
+        nextItem(currentItem);
+    }
+});
+
+console.log('hi')
